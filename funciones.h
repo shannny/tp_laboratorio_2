5bbd3ce4064d2adfbbd9c57a52481e2107b1a2ce
shannny <shanny_1991@hotmail.com>
2016-10-09 02:03:07 -0300

    Revert "tp_laboratorio_2"
    This reverts commit 2c3828c2d35182217913169138e8b1b57da0084d.

---
+++ funciones.h
@@ -0,0 +1,27 @@
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
+
+typedef struct {
+
+    char nombre[50];
+    int edad;
+    int estado;
+    int dni;
+
+}EPersona;
+
+
+int buscarPorDni(EPersona lista[], int dni);
+int menu();
+void inicializar(EPersona per[]);
+void altas(EPersona per[]);
+int buscar(EPersona per[],long int dni);
+void imprimir(EPersona per[]);
+void bajas(EPersona per[]);
+int libre(EPersona per[]);
+void grafico(EPersona per[]);
+void valtam(char x[],int tam);
+
+#endif // FUNCIONES_H_INCLUDED
+
+
